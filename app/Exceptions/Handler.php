26cb425f261decc33bd459105faad0c58019ebc2
arybagong2000 <arybagong2000@yahoo.com>
2023-08-19 10:58:46 +0700

    login ok

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Spatie\Permission\Exceptions\UnauthorizedException;
 
 class Handler extends ExceptionHandler
 {
@@ -44,5 +45,17 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+
+    public function render($request, Throwable $e)
+    {
+        if ($e instanceof UnauthorizedException) {
+            return response()->view("errors.index", [
+                "exception" => $e
+            ], 403);
+        }
+        
+        return parent::render($request, $e);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -26,8 +26,8 @@ Auth::routes();
   
 Route::get('/home', [HomeController::class, 'index'])->name('home');
   
-//Route::group(['middleware' => ['auth']], function() {
+Route::group(['middleware' => ['auth']], function() {
     Route::resource('roles', RoleController::class);
     Route::resource('users', UserController::class);
     Route::resource('products', ProductController::class);
-//});
+});
